44bf5a1052c581e53aa883c23b36576e8c656865
haolin <542538131@qq.com>
2017-03-03 18:29:22 +0800

    change

--- 645baowen.py
+++ 645baowen.py
@@ -11,12 +11,13 @@ B = int(B)
 C = int(C)
 def spilt1(x):
     L = [str(int(x) - (int(x / 100) * 100)),str(int(x/100)-(int(x/10000)*100)),
-       str(int(x/10000)-(int(x/1000000)*100)),str(int(x/1000000))]
+       str(int(x/10000)-(int(x/1000000)*100)),str(int(x/1000000))
+	   ]
     return L
 #eg：分离电表号，将1662233分离成33,22,66,01
 def spilt2(x):
     L = [ hex(int(str((int(x ) - int(x / 100) * 100)), 16) + 0x33),hex(int(str(int(x/10000)),16)+0x33),
-        hex(int(str((int(x/100)-int(x/10000)*100)),16)+0x33),
+        hex(int(str((int(x/100)-int(x/10000)*100)),16)+0x33)
        ]
     return L
 #eg:分离数据区，将993366分离成99,66,CC
@@ -25,7 +26,7 @@ L=spilt1(x)+['00','00','68','14','15','32','34','C3','37','35','33','33','33','3
 s=0
 for i in L:
     s=s+int(i,16)
-while s>=0xFF:
+while s>0xFF:
     s=s-0x100
 
 #校验和的运算判别
